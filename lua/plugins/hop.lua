d58d7d43a27b74a9fba5214aab06099fee959ff8
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2023-04-21 12:57:24 +0300

    update hop for visual

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -16,6 +16,8 @@ return {
       local directions = require("hop.hint").HintDirection
       -- you can configure Hop the way you like here; see :h hop-config
       hop.setup({ keys = "etovxqpdygfblzhckisuran" })
+
+      vim.keymap.set({ "v" }, "<leader>f", "<cmd>HopChar2<CR>", { remap = true })
     end,
   },
 }
